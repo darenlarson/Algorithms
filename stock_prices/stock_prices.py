e44921525327536eccc7a1139ea367f8f710b824
darenlarson <larson.daren@gmail.com>
2019-03-25 15:15:36 -0500

    stock_prices.py done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    # Initialize maxProfit variable
+    maxProfit = float("-infinity")
 
+    # Loop over entire list, setting the buyPrice
+    for index, buyPrice in enumerate(prices):
+        
+        # Loop over the list of prices that come after the buyPrice and set the sellPrice eachtime.
+        # Only need to look at the prices that come after the buyPrice since we must sell after buying.
+        for sellPrice in prices[index+1:]:
+            profit = sellPrice - buyPrice
+            if profit > maxProfit:
+                maxProfit = profit
+
+    return maxProfit
+
+            
+# print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
